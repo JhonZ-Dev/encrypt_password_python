cca62177474ffa35a56cc8cbaffe25dbcf75db52
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-12 21:04:05 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,11 @@
 import hashlib
+
+
+def cifrar_contrasenia(contrasenia):
+    """Cifra la contraseña con el algoritmo sha256 y la devuelve en formato hexadecimal"""
+    
+    
+    #crear un objeti has de la clase hashlib
+    sha256 = hashlib.sha256()
+    
+    
